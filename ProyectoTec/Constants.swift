5c937c6fd06a4a2f3823abaa7afdbf5ee2e2fd74
Alex Mejicanos <alexmejicanos@outlook.com>
2017-03-15 12:39:38 -0600

    Paso 2 ( Creación Constantes y Navigation Bar ) - Agrego Archivo Constante - Modifico UIApparence para Navigation Bar - Modifico Info.plist para cambiar StatusBar

---
+++ ProyectoTec/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ProyectoTec
+//
+//  Created by Alex Mejicanos on 15/03/17.
+//  Copyright © 2017 Alex Mejicanos. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Constants {
+    static let baseColor: UIColor = UIColor(colorLiteralRed: 26/255.0, green: 121/255.0, blue: 68/255.0, alpha: 1.0)
+    static let textColorNavigation: UIColor = UIColor.white
+}
