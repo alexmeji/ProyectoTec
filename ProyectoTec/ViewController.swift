9580e29825fa710a0cbbd913b1fa4424b9078e05
Alex Mejicanos <alexmejicanos@outlook.com>
2017-03-15 12:14:03 -0600

    Paso 1 (Creamos Estructura Carpetas Proyecto)

---
+++ ProyectoTec/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProyectoTec
+//
+//  Created by Alex Mejicanos on 15/03/17.
+//  Copyright © 2017 Alex Mejicanos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
