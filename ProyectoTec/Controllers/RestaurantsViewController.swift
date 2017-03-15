edec8e34a90c8ecb49dcf3059ee8dd0e637ab4cd
Alex Mejicanos <alexmejicanos@outlook.com>
2017-03-15 16:47:05 -0600

    Paso 6 (Creación de Tabla + Información)

--- ProyectoTec/Controllers/RestaurantsViewController.swift
+++ ProyectoTec/Controllers/RestaurantsViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class RestaurantsViewController: UIViewController {
+class RestaurantsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    var restaurants: NSArray? = nil
+    @IBOutlet var tableInfo: UITableView?
+    
+    var restaurants: NSArray? = NSArray()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         loadJSONInfo()
+        tableInfo?.delegate = self
+        tableInfo?.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +27,79 @@ class RestaurantsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (restaurants?.count)!
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 225
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath) as! RestaurantCell
+        let restaurantInfo: [String: AnyObject] = restaurants![indexPath.row] as! [String: AnyObject]
+        
+        print(restaurantInfo)
+        
+        cell.picture?.image = UIImage(named: restaurantInfo["image"] as! String)
+        cell.name?.text = restaurantInfo["name"] as? String
+        cell.info?.text = restaurantInfo["description"] as? String
+        
+        let starCount: Int = (restaurantInfo["stars"] as! Int)
+        let priceCount: Int = (restaurantInfo["price"] as! Int)
+        
+        cell.star_1?.isHidden = true
+        cell.star_2?.isHidden = true
+        cell.star_3?.isHidden = true
+        cell.star_4?.isHidden = true
+        cell.star_5?.isHidden = true
+        
+        cell.money_1?.isHidden = true
+        cell.money_2?.isHidden = true
+        cell.money_3?.isHidden = true
+        cell.money_4?.isHidden = true
+        cell.money_5?.isHidden = true
+        
+        for x in 1...starCount {
+            switch x {
+            case 1:
+                cell.star_1?.isHidden = false
+            case 2:
+                cell.star_2?.isHidden = false
+            case 3:
+                cell.star_3?.isHidden = false
+            case 4:
+                cell.star_4?.isHidden = false
+            case 5:
+                cell.star_5?.isHidden = false
+            default:
+                cell.star_5?.isHidden = false
+            }
+        }
+        
+        for x in 1...priceCount {
+            switch x {
+            case 1:
+                cell.money_1?.isHidden = false
+            case 2:
+                cell.money_2?.isHidden = false
+            case 3:
+                cell.money_3?.isHidden = false
+            case 4:
+                cell.money_4?.isHidden = false
+            case 5:
+                cell.money_5?.isHidden = false
+            default:
+                cell.money_5?.isHidden = false
+            }
+        }
+        
+        return cell
+    }
 
     func loadJSONInfo() -> Void {
         if let path = Bundle.main.path(forResource: "info", ofType: "json") {
